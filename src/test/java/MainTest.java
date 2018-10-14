08c0f1a5216ce0df12e0638051cdbe2380dea1ee
Abigail Lu <abigailitliong.lu@calbaptist.edu>
2018-10-14 14:41:57 -0700

    testing out junit testing

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,4 +1,13 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.*;
 
-@
 public class MainTest {
+
+    @Test
+    public void testMain() {
+        System.out.println("Hello, World");
+
+    }
+
 }
