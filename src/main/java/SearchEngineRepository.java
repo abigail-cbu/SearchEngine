19f6775afac1b6d76c72645599491b98b8265e19
Abigail Lu <abigailitliong.lu@calbaptist.edu>
2018-10-14 18:32:45 -0700

    Working on getting jbdc inot project

--- src/main/java/SearchEngineRepository.java
+++ src/main/java/SearchEngineRepository.java
@@ -4,22 +4,26 @@ public class SearchEngineRepository {
 
     private String _myDrive = "com.mysql.jdbc.Driver";
 
+    // MySQL: "jdbc:mysql://hostname:port/databaseName", "username", "password"
     private String _myUrl = "jdbc:mysql://localhost:3306/SearchEngineDB";
 
     public void InsertLink(String linkText, String url, int prevLinkID) {
         try {
-            Class.forName(_myDrive);
+            //Class.forName(_myDrive);
             Connection _globalConnectionString = DriverManager.getConnection(_myUrl, "root", "");
 
             // the mysql insert statement
-            String query = "INSERT INTO LINKS (LinkText, URL, PrevLinkID)"
-                    + "VALUES (?, ?, ?)";
+//            String query = "INSERT INTO LINKS (LinkText, URL, PrevLinkID)"
+//                    + "VALUES (?, ?, ?)";
+//
+//            // create the mysql insert and add parameters
+//            PreparedStatement preparedStmt = _globalConnectionString.prepareStatement(query);
+//            preparedStmt.setString(1, linkText);
+//            preparedStmt.setString(2, url);
+//            preparedStmt.setInt(3, prevLinkID);
 
-            // create the mysql insert preparedstatement
+            String query = "select * from Links";
             PreparedStatement preparedStmt = _globalConnectionString.prepareStatement(query);
-            preparedStmt.setString(1, linkText);
-            preparedStmt.setString(2, url);
-            preparedStmt.setInt(3, prevLinkID);
 
             // execute the preparedstatement
             preparedStmt.execute();
