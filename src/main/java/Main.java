56e4a984261631c0732baf949513d0e6ad7d46af
abigail-cbu <43055638+abigail-cbu@users.noreply.github.com>
2018-10-25 19:16:04 -0700

    [Abigail] Setup initial database and insert data from web crawler  (#6)
    * [Abigail] added maven with logging and jdbc. trying to setup database
    
    * testing out junit testing
    
    * Added the repository for real
    
    * Working on getting jbdc inot project
    
    * upgraded version of jbdc connector in pom file
    
    * I was able to insert data into the Links table. Yay

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,7 +9,7 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
 
-        Website CBU = new Website("CalBaptist","https://calbaptist.edu/");
+        Website CBU = new Website("CalBaptist", "https://calbaptist.edu/");
 
         PrintWriter pw = new PrintWriter(new File("test.csv"));
         StringBuilder sb = new StringBuilder();
@@ -22,23 +22,24 @@ public class Main {
 
         final Document doc = Jsoup.connect(CBU.getUrl()).get();
         String temp;
-        for(Element l:doc.select("a[href]"))
-        {
+        for (Element l : doc.select("a[href]")) {
             CBU.linkCountPlusOne();
-            CBU.setLinkNames(CBU.getLinkCount(),l.text());
-            if(l.attr("href").startsWith("/")){
+            CBU.setLinkNames(CBU.getLinkCount(), l.text());
+            if (l.attr("href").startsWith("/")) {
                 temp = l.attr("href");
-                temp = CBU.getUrl()+temp.substring(1);
-                CBU.setLinks(CBU.getLinkCount(),temp);
-            }
-            else
-                CBU.setLinks(CBU.getLinkCount(),l.attr("href"));
+                temp = CBU.getUrl() + temp.substring(1);
+                CBU.setLinks(CBU.getLinkCount(), temp);
+            } else
+                CBU.setLinks(CBU.getLinkCount(), l.attr("href"));
         }
+
            /* Elements links = doc.select("a[href]"); // a with href
             Elements pngs = doc.select("img[src$=.png]");
             // img with src ending .png
             int counter =1;
             Element masthead = doc.select("div.masthead").first();*/
+
+        /*
         for(int i = 0;i<CBU.getLinkCount();i++){
 
             //System.out.println(CBU.getLinkNames().get(i)+":      "+CBU.getLinks().get(i));
@@ -54,5 +55,12 @@ public class Main {
 
         pw.write(sb.toString());
         pw.close();
+        */
+
+        for (int i = 0; i < CBU.getLinkCount(); i++) {
+            SearchEngineRepository ser = new SearchEngineRepository();
+            ser.InsertLink(CBU.getLinkNames().get(i + 1), CBU.getLinks().get(i + 1), 0);
+        }
+
     }
 }
