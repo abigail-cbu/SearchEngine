16f7eb8270246dd4a4dade224fbce028216b7b3b
Abigail Lu <abigailitliong.lu@calbaptist.edu>
2018-11-04 17:13:40 -0800

    implemented Search Engine Repository with Web Crawler

--- src/main/java/Classes/SearchEngineRepository.java
+++ src/main/java/Classes/SearchEngineRepository.java
@@ -1,10 +1,11 @@
-
+package Classes;
 
 import com.mysql.cj.protocol.Resultset;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.sql.*;
+import java.util.ArrayList;
 
 public class SearchEngineRepository {
 
@@ -15,7 +16,7 @@ public class SearchEngineRepository {
 
     public static final Logger logger = LogManager.getLogger(SearchEngineRepository.class);
 
-    public void InsertLink(String linkText, String url, int prevLinkID, int depth) {
+    public void InsertWebsite(String linkText, String url, int depth) {
         try {
             Class.forName(_myDrive);
 
@@ -23,16 +24,15 @@ public class SearchEngineRepository {
             logger.info("Connected to SearchEngineDB");
 
             //the mysql insert statement
-            String query = "INSERT INTO LINKS (LinkText, URL, PrevLinkID, Crawled, Depth)"
-                    + "VALUES (?, ?, ?, ?, ?)";
+            String query = "INSERT INTO Websites (LinkText, URL, Crawled, Depth)"
+                    + "VALUES (?, ?, ?, ?)";
 
             // create the mysql insert and add parameters
             PreparedStatement preparedStmt = _globalConnectionString.prepareStatement(query);
             preparedStmt.setString(1, linkText);
             preparedStmt.setString(2, url);
-            preparedStmt.setInt(3, prevLinkID);
-            preparedStmt.setBoolean(4, false); // should be first time inserting link
-            preparedStmt.setInt(5, depth);
+            preparedStmt.setBoolean(3, false); // should be first time inserting link
+            preparedStmt.setInt(4, depth);
 
 
             // execute the preparedstatement
@@ -45,7 +45,7 @@ public class SearchEngineRepository {
         }
     }
 
-    public void UpdateLink(String url) {
+    public void UpdateWebsite(String url) {
         /// update if we have crawled the url before
         try {
             Class.forName(_myDrive);
@@ -54,7 +54,7 @@ public class SearchEngineRepository {
             logger.info("Connected to SearchEngineDB");
 
             //the mysql insert statement
-            String query = "UPDATE LINKS"
+            String query = "UPDATE Websites"
                     + "SET Crawled = TRUE" +
                     "   WHERE URL = ?";
 
@@ -72,7 +72,7 @@ public class SearchEngineRepository {
         }
     }
 
-    public Boolean IsCrawled(String url) {
+    public Boolean CheckIfIsCrawled(String url) {
         // check if url has been crawled
         Boolean isCrawled = false; // it is okay if we have to crawl a url again
         try {
@@ -83,7 +83,7 @@ public class SearchEngineRepository {
 
             //the mysql insert statement
             String query = "SELECT Crawled"
-                    + "FROM Links" +
+                    + "FROM Websites" +
                     "WHERE URL = ?";
 
             // create the mysql insert and add parameters
@@ -91,7 +91,12 @@ public class SearchEngineRepository {
             preparedStmt.setString(1, url);
 
             // execute the preparedstatement
-            Boolean value = preparedStmt.execute();
+            ResultSet rs = preparedStmt.executeQuery();
+
+            // loop through the result set
+            while (rs.next()) {
+                isCrawled = rs.getBoolean("Crawled");
+            }
 
             _globalConnectionString.close();
 
@@ -102,7 +107,33 @@ public class SearchEngineRepository {
         return isCrawled;
     }
 
-    public void InsertSourceCode(String url) {
+    public void SetCrawled(String url) {
+        try {
+            Class.forName(_myDrive);
+
+            Connection _globalConnectionString = DriverManager.getConnection(_myUrl, "root", "");
+            logger.info("Connected to SearchEngineDB");
+
+            //the mysql insert statement
+            String query = "UPDATE Websites"
+                    + "SET Crawled = TRUE" +
+                    " WHERE URL= ?";
+
+            // create the mysql insert and add parameters
+            PreparedStatement preparedStmt = _globalConnectionString.prepareStatement(query);
+            preparedStmt.setString(1, url);
+
+            // execute the preparedstatement
+            preparedStmt.execute();
+
+            _globalConnectionString.close();
+
+        } catch (Exception ex) {
+            logger.error(ex.getMessage());
+        }
+    }
+
+    public void InsertSourceCode(String url, String sourceCode) {
         // insert source code for specified url
 
         try {
@@ -117,13 +148,14 @@ public class SearchEngineRepository {
                     "WHERE LinkID = " +
                     "(" +
                     "   SELECT LinkID" +
-                    "   FROM Links" +
+                    "   FROM Websites" +
                     "   WHERE URL = ?" +
                     ")";
 
             // create the mysql insert and add parameters
             PreparedStatement preparedStmt = _globalConnectionString.prepareStatement(query);
-            preparedStmt.setString(1, url);
+            preparedStmt.setString(1, sourceCode);
+            preparedStmt.setString(2, url);
 
             // execute the preparedstatement
             preparedStmt.execute();
@@ -133,6 +165,44 @@ public class SearchEngineRepository {
         } catch (Exception ex) {
             logger.error(ex.getMessage());
         }
+    }
+
+    public Boolean WebsiteExists(String url) {
+        // check if url has been crawled
+        ArrayList<String> websites = new ArrayList<String>();
+        try {
+            Class.forName(_myDrive);
+
+            Connection _globalConnectionString = DriverManager.getConnection(_myUrl, "root", "");
+            logger.info("Connected to SearchEngineDB");
+
+            //the mysql insert statement
+            String query = "SELECT URL"
+                    + "FROM Websites" +
+                    "WHERE URL = ?";
+
+            // create the mysql insert and add parameters
+            PreparedStatement preparedStmt = _globalConnectionString.prepareStatement(query);
+            preparedStmt.setString(1, url);
+
+            // execute the preparedstatement
+            ResultSet rs = preparedStmt.executeQuery();
+
+            // loop through the result set
+            while (rs.next()) {
+                websites.add(rs.getString("URL"));
+            }
+
+            if (websites.size() > 0)
+                return true;
+
+            _globalConnectionString.close();
+
+        } catch (Exception ex) {
+            logger.error(ex.getMessage());
+        }
+
+        return false;
     }
 
 }
--- src/main/java/Threads/Crawling.java
+++ src/main/java/Threads/Crawling.java
@@ -1,5 +1,6 @@
 package Threads;
 
+import Classes.SearchEngineRepository;
 import Classes.Website;
 import Main.Main;
 import org.apache.logging.log4j.LogManager;
@@ -17,7 +18,8 @@ public class Crawling implements Runnable {
     private Website page;
     private StringBuilder sb;
     public static final Logger logger = LogManager.getLogger(Crawling.class);
-    public Crawling(Website pWebsite,int pID){
+
+    public Crawling(Website pWebsite, int pID) {
         this.threadId = pID;
         page = pWebsite;
         sb = new StringBuilder();
@@ -28,70 +30,80 @@ public class Crawling implements Runnable {
         sb.append("Link");
         sb.append('\n');
     }
+
     public int getId() {
         return this.threadId;
     }
+
     public void run() {
-        if(page.getDepth()>3 ){
+        if (page.getDepth() > 0) {
             return;
         }
 
         // saves web page as a doc
         try {
             final Document doc = Jsoup.connect(page.getUrl()).get();
-            String temp;
-            for(Element l:doc.select("a[href]"))
-            {
+            String elementUrl; // for urls that are pointing to home page (i.e. /apply == calbaptist.edu/apply)
+            SearchEngineRepository ser = new SearchEngineRepository();
+
+            for (Element l : doc.select("a[href]")) {
                 Website w;
                 page.linkCountPlusOne();
-                if(l.attr("href").startsWith("/")){
-                    temp = l.attr("href");
-                    temp = page.getUrl()+temp.substring(1);
-                    w= new Website(l.text(),temp,page.getDepth()+1);
+                int newDepth = page.getDepth() + 1;
+
+                if (l.attr("href").startsWith("/")) {
+                    elementUrl = l.attr("href");
+                    elementUrl = page.getUrl() + elementUrl.substring(1);
+                    w = new Website(l.text(), elementUrl, newDepth);
+
+                } else {
+                    w = new Website(l.text(), l.attr("href"), newDepth);
                 }
-                else
-                    w= new Website(l.text(),l.attr("href"),page.getDepth()+1);
-                page.addToInnerWebsites(w);
-                if(!Main.urlStrings.contains(w.getUrl())) {
-                    Main.urlStrings.add(w.getUrl());
+
+                //page.addToInnerWebsites(w); // todo: we may not need this... -Abby
+
+                // 11/4/2018: saving websites to database
+//                if (!Main.urlStrings.contains(w.getUrl())) {
+//                    Main.urlStrings.add(w.getUrl());
+//                    Main.sitesToCrawl.add(w);
+//                    // logger.info("   name:"+w.getSiteName()+"   URL"+w.getUrl()+"   depth:"+w.getDepth()+"  isCrawled:"+w.getIsCrawled());
+//                }
+
+                // only save unique websites
+                if (!ser.WebsiteExists(w.getUrl())) {
+                    ser.InsertWebsite(w.getSiteName(), w.getUrl(), w.getDepth());
                     Main.sitesToCrawl.add(w);
-                   // logger.info("   name:"+w.getSiteName()+"   URL"+w.getUrl()+"   depth:"+w.getDepth()+"  isCrawled:"+w.getIsCrawled());
                 }
-
-
             }
-            for(int i = 0;i<page.getLinkCount();i++){
-                sb.append(i+1);
-                sb.append(',');
-              //  sb.append(page.getLinkNames().get(i+1));
-                sb.append(',');
-              //  sb.append(page.getLinks().get(i+1));
-                sb.append('\n');
 
-            }
-            page.setBodyText(doc.body().text());
+            page.setSourceCode(doc.body().text());
+            ser.InsertSourceCode(page.getUrl(), doc.body().text());
+
             page.isCrawled();
+            ser.SetCrawled(page.getUrl());
 
-        }catch (Exception e){
-            
+        } catch (Exception e) {
+            logger.error(e.getMessage());
         }
-        
+
     }
+
     public boolean isDone() {
         return this.isDone;
     }
-    public void toFile (String pPathName){
+
+    public void toFile(String pPathName) {
         try {
             PrintWriter pw = new PrintWriter(new File(pPathName));
             pw.write(sb.toString());
             pw.close();
-        }catch (Exception e){
-
+        } catch (Exception e) {
+            logger.error(e.getMessage());
         }
 
     }
-    public Website nextCrawl()
-    {
+
+    public Website nextCrawl() {
         return page;
 
     }
